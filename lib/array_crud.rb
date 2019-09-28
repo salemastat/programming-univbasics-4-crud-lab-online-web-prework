7a4025f9ac3737cd3663d65bcd8c8bfea7ed1c38
Salem Asrat <salemasrat@gmail.com>
2019-09-28 03:39:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,13 @@
 def create_an_empty_array
+  []
+end
+ 
+
+#def create_an_array
   
 end
 
-def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
-end
+ end
 
 def add_element_to_start_of_array(array, element)
   
